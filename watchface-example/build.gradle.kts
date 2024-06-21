691ecdb4537b36c0b34be1c75bddb26242d23baa
dudu <duyuao200849@163.com>
2024-06-22 01:35:53 +0800

    表盘构建脚本

--- watchface-example/build.gradle.kts
+++ watchface-example/build.gradle.kts
@@ -35,3 +35,30 @@ dependencies {
     //watchface-dev
     implementation(project(":watchface-dev-utils"))
 }
+
+tasks.register<Zip>("packageDebugWatchface") {
+    dependsOn("assembleDebug")
+    doFirst{
+        val original = file("${buildDir}/outputs/apk/debug/${project.name}-debug.apk")
+        val wf = file("${buildDir}/outputs/apk/debug/${project.name}.wf")
+        file("${buildDir}/outputs/apk/debug/output-metadata.json").delete()
+        original.renameTo(wf)
+    }
+    from("${buildDir}/outputs/apk/debug")
+    archiveFileName.set("${project.name}.zip")
+    destinationDirectory.set(file("${buildDir}/outputs/watchface/debug"))
+    
+}
+
+tasks.register<Zip>("packageReleaseWatchface") {
+    dependsOn("assembleRelease")
+    doFirst{
+        val original = file("${buildDir}/outputs/apk/release/${project.name}-release.apk")
+        val wf = file("${buildDir}/outputs/apk/release/${project.name}.wf")
+        file("${buildDir}/outputs/apk/release/output-metadata.json").delete()
+        original.renameTo(wf)
+    }
+    from("${buildDir}/outputs/apk/release")
+    archiveFileName.set("${project.name}.zip")
+    destinationDirectory.set(file("${buildDir}/outputs/watchface/release"))
+}
