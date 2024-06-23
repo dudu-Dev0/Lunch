88136c0e1e14b24fb0a38bba319b76744e6b94ca
dudu <duyuao200849@163.com>
2024-06-23 14:06:06 +0800

    表盘构建脚本优化

--- watchface-example/build.gradle.kts
+++ watchface-example/build.gradle.kts
@@ -1,13 +1,11 @@
 
 plugins {
     id("com.android.application")
-    
 }
 
 android {
     namespace = "com.dudu.watchface.example"
-    compileSdk = 33
-    
+    compileSdk = 33 
     defaultConfig {
         applicationId = "com.dudu.watchface.example"
         minSdk = 19
@@ -36,29 +34,86 @@ dependencies {
     implementation(project(":watchface-dev-utils"))
 }
 
+
+//表盘在桌面内显示的名称
+val watchfaceName = "数码太空人"
+//表盘作者
+val author = "dudu"
+    
 tasks.register<Zip>("packageDebugWatchface") {
-    dependsOn("assembleDebug")
+    group = "build"
+    description = "构建debug表盘文件"
+    dependsOn("assembleDebug","copyPreviewImageDebug")
     doFirst{
-        val original = file("${buildDir}/outputs/apk/debug/${project.name}-debug.apk")
+        val apkDir = file("${buildDir}/outputs/apk/debug")
+        val original = apkDir.walkTopDown().filter { it.extension == "apk" }.toList().get(0);
         val wf = file("${buildDir}/outputs/apk/debug/${project.name}.wf")
         file("${buildDir}/outputs/apk/debug/output-metadata.json").delete()
+        println("Creating WF...")
         original.renameTo(wf)
+        val manifestContent = """
+            {
+                "name": "${project.name}",
+                "packageName": "${android.defaultConfig.applicationId}",
+                "displayName": "${watchfaceName}"
+                "versionCode": ${android.defaultConfig.versionCode}
+                "author": "${author}"
+            }
+        """.trimIndent()
+        val manifestFile = file("${buildDir}/outputs/apk/debug/manifest.json")
+        manifestFile.writeText(manifestContent)
+        println("Write manifest file SUCCESSFUL")
     }
     from("${buildDir}/outputs/apk/debug")
     archiveFileName.set("${project.name}.zip")
     destinationDirectory.set(file("${buildDir}/outputs/watchface/debug"))
-    
+    doLast{
+        println("Watchface created successful at ${buildDir}/outputs/watchface/debug/${project.name}.zip !")
+    }
 }
 
 tasks.register<Zip>("packageReleaseWatchface") {
-    dependsOn("assembleRelease")
+    group = "build"
+    description = "构建release表盘文件"
+    dependsOn("assembleRelease","copyPreviewImageRelease")
     doFirst{
-        val original = file("${buildDir}/outputs/apk/release/${project.name}-release.apk")
+        val apkDir = file("${buildDir}/outputs/apk/release")
+        val original = apkDir.walkTopDown().filter { it.extension == "apk" }.toList().get(0);
         val wf = file("${buildDir}/outputs/apk/release/${project.name}.wf")
         file("${buildDir}/outputs/apk/release/output-metadata.json").delete()
+        println("Creating WF...")
         original.renameTo(wf)
+        val manifestContent = """
+            {
+                "name": "${project.name}",
+                "packageName": "${android.defaultConfig.applicationId}",
+                "displayName": "${watchfaceName}"
+                "versionCode": ${android.defaultConfig.versionCode}
+                "author": "${author}"
+            }
+        """.trimIndent()
+        val manifestFile = file("${buildDir}/outputs/apk/release/manifest.json")
+        manifestFile.writeText(manifestContent)
+        println("Write manifest file SUCCESSFUL")
     }
     from("${buildDir}/outputs/apk/release")
     archiveFileName.set("${project.name}.zip")
     destinationDirectory.set(file("${buildDir}/outputs/watchface/release"))
+    doLast{
+        println("Watchface created successful at ${buildDir}/outputs/watchface/release/${project.name}.zip !")
+    }
 }
+tasks.register<Copy>("copyPreviewImageDebug") {
+    mustRunAfter("createDebugApkListingFileRedirect")
+    from("src/main/assets")
+    into("${buildDir}/outputs/apk/debug")
+    include("preview.png")
+}
+
+tasks.register<Copy>("copyPreviewImageRelease") {
+    mustRunAfter("createReleaseApkListingFileRedirect")
+    from("src/main/assets")
+    into("${buildDir}/outputs/apk/release")
+    include("preview.png")
+}
+
