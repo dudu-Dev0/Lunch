0569b8169885ba8a9c8442b8105be771a75e6a4a
dudu <duyuao200849@163.com>
2024-06-30 23:10:01 +0800

    为表盘内嵌类似activity的方法做准备

---
+++ watchface-dev-utils/src/main/java/com/dudu/wearlauncher/model/WatchSurface.java
@@ -0,0 +1,57 @@
+package com.dudu.wearlauncher.model;
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public abstract class WatchSurface extends ViewGroup{
+    /**
+    *在表盘中实现类似activity的效果
+    */
+    private String path; //表盘路径(.wf)
+    private Context context;
+    private Resources resources;
+
+    public WatchSurface(Context context,String path) {
+        this(context,null,path);
+    }
+
+    public WatchSurface(Context context, AttributeSet attributeSet,String path) {
+        this(context,attributeSet,0,path);
+    }
+
+    public WatchSurface(Context context, AttributeSet attributeSet, int i,String path) {
+        super(context,attributeSet,i);
+        this.context = context;
+        this.path = path;
+        this.resources = initResources(context);
+    }
+    
+    public abstract void onCreate();
+    public abstract void onDestory();
+    
+    public Resources initResources(Context context) {
+        try {
+            AssetManager assetManager = AssetManager.class.newInstance();
+            assetManager.getClass().getMethod("addAssetPath", String.class).invoke(assetManager, path);
+            Resources resources = context.getResources();
+            return new Resources(assetManager, resources.getDisplayMetrics(), resources.getConfiguration());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public void setContentView(int id) {
+        LayoutInflater.from(context).inflate(getResources().getLayout(id),this);
+    }
+    
+    public abstract View getWatchSurface();
+
+    public Resources getResources() {
+        return this.resources == null ? super.getResources() : this.resources;
+    }
+}
