9c6d0f50e19aa8d4ee01e02a9c483a104136e423
dudu <duyuao200849@163.com>
2024-08-14 08:54:22 +0000

    添加水波纹，优化ui

--- app/src/main/java/com/dudu/wearlauncher/widget/SwitchIconButton.java
+++ app/src/main/java/com/dudu/wearlauncher/widget/SwitchIconButton.java
@@ -3,6 +3,8 @@ package com.dudu.wearlauncher.widget;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatImageButton;
@@ -21,10 +23,18 @@ public class SwitchIconButton extends AppCompatImageButton {
     public SwitchIconButton(@NonNull @NotNull Context context, @Nullable @org.jetbrains.annotations.Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         setActivated(false);
+        setForeground(getResources().getDrawable(R.drawable.ripple_effect));
         setBackground(getResources().getDrawable(R.drawable.circle_bg_gray));
         setOnClickListener(v -> {
             //Toast.makeText(context, "clicked", Toast.LENGTH_SHORT).show();
-            setActivated(!isActivated());
+            //setActivated(!isActivated());
+        });
+        setOnTouchListener((view, motionEvent) -> {
+            if (motionEvent.getAction() == MotionEvent.ACTION_UP){
+                super.performClick();
+                setActivated(!isActivated());
+            }
+            return false;
         });
         setScaleType(ScaleType.CENTER);
     }
