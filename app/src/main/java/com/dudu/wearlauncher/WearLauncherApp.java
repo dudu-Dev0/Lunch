827538bb76e92f0e2d138f62c7e1a17bad58d719
dudu <duyuao200849@163.com>
2024-08-29 03:50:33 +0000

    lock smallestScreenWidthDp

--- app/src/main/java/com/dudu/wearlauncher/WearLauncherApp.java
+++ app/src/main/java/com/dudu/wearlauncher/WearLauncherApp.java
@@ -1,15 +1,13 @@
 package com.dudu.wearlauncher;
+
 import android.app.Application;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Build;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.widget.Toast;
 import androidx.multidex.MultiDex;
 import com.blankj.utilcode.util.AppUtils;
-import com.blankj.utilcode.util.CrashUtils;
-import com.blankj.utilcode.util.RomUtils;
 import com.dudu.wearlauncher.utils.SharedPreferencesUtil;
 import com.tencent.bugly.crashreport.CrashReport;
 public class WearLauncherApp extends Application {
@@ -38,13 +36,13 @@ public class WearLauncherApp extends Application {
      */
     public static Context getFitDisplayContext(Context old){
         Context newContext = old;
-        
-        float density = context.getResources().getDisplayMetrics().widthPixels/360; //获取放大dpi倍数
-        
+
+        float density = (float) context.getResources().getDisplayMetrics().widthPixels / 360; //获取放大dpi倍数
         try{
             //DisplayMetrics displayMetrics = old.getResources().getDisplayMetrics();
             Configuration configuration = old.getResources().getConfiguration();
-            configuration.densityDpi = (int)(/*displayMetrics.densityDpi*/320 * density);    //锁死dpi为320，防止设备默认dpi不同导致界面出问题
+            configuration.smallestScreenWidthDp = 320;
+            configuration.densityDpi = (int) (/*displayMetrics.densityDpi*/ 320 * density);    //锁死dpi为320，防止设备默认dpi不同导致界面出问题
             newContext = old.createConfigurationContext(configuration);
         }catch (Exception e){
             Toast.makeText(newContext, "调整缩放失败, 请联系开发者", Toast.LENGTH_SHORT).show();
