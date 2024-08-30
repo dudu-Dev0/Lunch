9f968d4f9109abb8ea6c3c56fa112f215bcde861
dudu <duyuao200849@163.com>
2024-08-30 20:11:58 +0800

    重要修复，返回逻辑修改

--- app/src/main/java/com/dudu/wearlauncher/ui/home/HomeActivity.java
+++ app/src/main/java/com/dudu/wearlauncher/ui/home/HomeActivity.java
@@ -3,6 +3,7 @@ package com.dudu.wearlauncher.ui.home;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import androidx.fragment.app.Fragment;
@@ -81,9 +82,15 @@ public class HomeActivity extends BaseActivity {
     protected void onDestroy() {
         super.onDestroy();
     }
-
+    @Override
+    public void onBackPressed() {
+        if(!isDestroyed()) {
+        	homeViewPager.setCurrentItem(0);
+        }
+    }
+    
     private void checkIsXTC() {
-        if (!RomUtils.getRomInfo().getName().contains("XTC")) {
+        if (!Build.BRAND.equalsIgnoreCase("XTC")) {
             PackageManager pm = getPackageManager();
             ComponentName originComponentName = new ComponentName(this, com.xtc.i3launcher.module.home.view.activity.HomeActivity.class);
             ComponentName disabledComponentName = new ComponentName(this, "com.dudu.wearlauncher.XTCHomeDisabled");
