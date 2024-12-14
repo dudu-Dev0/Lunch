fc325b7dfcdf8c933e18f76521cc3379d431cf62
dudu <duyuao200849@163.com>
2024-12-14 21:14:25 +0800

    添加签名

--- watchface-example/build.gradle.kts
+++ watchface-example/build.gradle.kts
@@ -20,14 +20,25 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    buildFeatures.buildConfig=true
+    
+    signingConfigs {
+        create("lunch-community") {
+            keyAlias = "Lunch Community"
+            keyPassword = "lunch-is-the-best"
+            storeFile = file("../lunch-community.keystore")
+            storePassword = "lunch-is-the-best"
+        }
+    }
     buildTypes {
+        debug {
+            signingConfig = signingConfigs.getByName("lunch-community")
+        }
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("lunch-community")
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
-    
 }
 
 dependencies {
